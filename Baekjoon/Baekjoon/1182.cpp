0e37969da39aed698fce86d63220a8f8e10c6a73
윤규 정 <asdf99245@naver.com>
2020-11-30 16:26:02 +0900

    1182 baekjoon

---
+++ Baekjoon/Baekjoon/1182.cpp
@@ -0,0 +1,38 @@
+//부분 수열의 합
+//https://www.acmicpc.net/problem/1182
+
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int N, S;
+int seq[21];
+int cnt = 0;
+
+void solve(int d, int sum) {
+	if (d == N) {  // 수열 길이와 같아졌을때
+		if (sum  == S) {  // sum이 S와 같다면 카운트 올려줌
+			cnt++;
+		}
+		return;
+	}
+
+	int i;
+
+	solve(d + 1, sum); // d번째 수를 안 더해줄때
+	solve(d + 1, sum + seq[d]); // d번째 수를 더할때
+	return;
+}
+int main() {
+	int i;
+	scanf("%d %d", &N, &S);
+
+	for (i = 0; i < N; i++) {
+		scanf("%d", &seq[i]);
+	}
+
+	solve(0, 0);
+	if (S == 0) cnt -= 1; //  S가 0 이면 아무것도 더하지않은 경우의 수 빼줌
+	printf("%d", cnt);
+	return 0;
+}
--- Baekjoon/Baekjoon/2468.cpp
+++ Baekjoon/Baekjoon/2468.cpp
@@ -1,5 +1,5 @@
 //안전영역
-#include <cstdio>
+/*#include <cstdio>
 #include <algorithm>
 #include <memory.h>
 #include <vector>
@@ -12,14 +12,14 @@ int dy[] = { 0,0,-1,1 };
 int dx[] = { 1,-1,0,0 };
 
 void DFS(int y, int x, int height) {
-	visited[y][x] = true;
+	visited[y][x] = true; // 방문하였으므로 true
 	int i;
 
 	for (i = 0; i < 4; i++) {
 		int ny = y + dy[i];
 		int nx = x + dx[i];
 		if (0 <= nx && nx < n && 0 <= ny && ny < n && visited[ny][nx] == false) {
-			if (field[ny][nx] > height) {
+			if (field[ny][nx] > height) { // 수위보다 높은곳일때 DFS
 				DFS(ny, nx, height);
 			}
 		}
@@ -29,7 +29,7 @@ void DFS(int y, int x, int height) {
 }
 int main() {
 	int i, j, k;
-	int maxHeight = -1; int minHeight = 9999;
+	int maxHeight = -1; 
 	scanf("%d", &n);
 
 	for (i = 0; i < n; i++) {
@@ -37,26 +37,24 @@ int main() {
 			scanf("%d", &field[i][j]);
 			if (field[i][j] > maxHeight)
 				maxHeight = field[i][j];
-			if (field[i][j] < minHeight)
-				minHeight = field[i][j];
 		}
 	}
 
 	int maxCnt = -1;
-	for (i = 0; i <= maxHeight; i++) {
+	for (i = 0; i <= maxHeight; i++) { // 하나도 안 왔을때부터 최대 높이까지
 		memset(visited, false, sizeof(visited));
 		int cnt = 0;
 		for (j = 0; j < n; j++) {
 			for (k = 0; k < n; k++) {
-				if (visited[j][k] == false && field[j][k] > i) {
+				if (visited[j][k] == false && field[j][k] > i) { // 방문했고 수위보다 높을때
 					DFS(j, k, i);
-					cnt++;
+					cnt++; // 영역의 개수올려줌
 				}
 			}
 		}
-		if (maxCnt < cnt) maxCnt = cnt;
+		if (maxCnt < cnt) maxCnt = cnt; // 최대영역 개수 갱신
 	}
 
 	printf("%d", maxCnt);
 	return 0;
-}
+}*/
