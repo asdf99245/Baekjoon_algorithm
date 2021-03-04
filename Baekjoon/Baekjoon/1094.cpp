764c3e74d90d92efdb0272b09f7ebce3283c536d
asdf99245 <asdf99245@naver.com>
2021-03-04 15:43:26 +0900

    11723번 집합 비트마스크

--- Baekjoon/Baekjoon/1094.cpp
+++ Baekjoon/Baekjoon/1094.cpp
@@ -1,7 +1,7 @@
 //막대기
 //https://www.acmicpc.net/problem/1094
 
-#include <cstdio>
+/*#include <cstdio>
 #include <algorithm>
 //#include <intrin.h> __popcnt() 이용 위한 라이브러리
 using namespace std;
@@ -18,8 +18,8 @@ int main() {
 
 	printf("%d", bitCount(X));
 	
-	/* 비주얼 스튜디오에 __popcnt() 함수로 하드웨어명령을 통해 빠르게 처리 가능하다
-	gcc 에서는 __builtin_popcount() 이용 */
+	//비주얼 스튜디오에 __popcnt() 함수로 하드웨어명령을 통해 빠르게 처리 가능하다
+	//gcc 에서는 __builtin_popcount() 이용
 	//printf("%d", __popcnt(X));
 	return 0;
-}
+}*/
