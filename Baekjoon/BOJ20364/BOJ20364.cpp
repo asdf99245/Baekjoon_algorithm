e2b5bb40416e3e313a420978291ecca3ae4258b7
asdf99245 <asdf99245@naver.com>
2021-05-28 17:54:43 +0900

    BOJ1562 계단 수 solve

---
+++ Baekjoon/BOJ20364/BOJ20364.cpp
@@ -0,0 +1,24 @@
+//부동산 다툼
+//https://www.acmicpc.net/problem/20364
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#define MAX 1 << 20
+using namespace std; 
+
+int arr[MAX + 1];
+bool check[MAX + 1];
+
+int main() {
+	int n, q, i;
+	scanf("%d %d", &n, &q);
+	for (i = 0; i < q; i++) {
+		int a;
+		scanf("%d", &a);
+
+	}
+
+	return 0;
+}
