5060cfcec2833efa7732612b4625af046ff0604f
asdf99245 <asdf99245@naver.com>
2021-08-05 17:55:33 +0900

    BOJ 11812 K진 트리

---
+++ Baekjoon/BOJ11812/BOJ11812.cpp
@@ -0,0 +1,39 @@
+// K진 트리
+// https://www.acmicpc.net/problem/11812
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main() {
+	long long int n, k, q;
+	scanf("%lld %lld %lld", &n, &k, &q);
+	for (int i = 0; i < q; i++) {
+		long long int x, y;
+		long long int cnt = 0;
+		scanf("%lld %lld", &x, &y);
+		if (k == 1) { // k == 1 일시 일렬로 연결돼있기때문에 노드 번호가 depth
+			printf("%lld\n", abs(y - x));
+		}
+		else { 
+			while (y != x) { // 같은 부모가 될때까지
+				// 높은 레벨의 노드를 부모노드로 바꾼다
+				if (y > x) {
+					y = (y - 2) / k + 1;
+				}
+				else {
+					x = (x - 2) / k + 1;
+				}
+				cnt++; // 거리 + 1
+			}
+
+			printf("%lld\n", cnt);
+		}
+	}
+
+
+	return 0;
+}
